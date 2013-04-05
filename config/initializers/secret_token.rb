53f9d526b6d04563100f663318c7b547ed4d7fbf
Jun Katagiri <katagiri.jun2@gmail.com>
2013-04-05 22:04:20 +0900

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterBot::Application.config.secret_key_base = 'efb1d6df43f6263300eb27ea1dcba03f5819f486d59f9f818e8cd0b562fa7f3d618711e2f8d40b9b4f9dd987fe0777c115f9da5578dffef202e2ffabed1c145c'
